88a3b7cd6f57694b03f6621b00a947fcb04cc6c7
MuhammadArham Shaikh <arhamshaikh128@gmail.com>
2026-04-11 22:42:59 +0500

    implemented singleton pattern in DBConnection.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("🚀 Initializing FlexDB...");
+        System.out.println("🚀 Initializing FitnessMS...");
 
         // Testing the connection
         Connection testConn = DBConnection.getConnection();
--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -4,16 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection{
+    private static Connection connection = null;
+
     private static final String URL = "jdbc:mysql://localhost:3306/FitnessMS";
     private static final String USER = "root";
     private static final String PASS = System.getenv("DB_PASSWORD");
 
-    public static Connection getConnection(){
-        try{
-            return DriverManager.getConnection(URL,USER,PASS);
-        } catch(Exception e){
-            System.out.println("❌ Connection Failed! Error: " + e.getMessage());
-            return null;
+    private DBConnection() {};
+
+   public static Connection getConnection() {
+        try {
+            // Only create a new connection if one doesn't exist or was closed
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASS);
+                System.out.println("🔌 New Database Connection Established.");
+            }
+        } catch (Exception e) {
+            System.out.println("❌ Connection Failed! " + e.getMessage());
         }
+        return connection;
     }
 }
